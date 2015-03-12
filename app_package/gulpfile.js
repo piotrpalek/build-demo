54c786f1991bb21b9aded4f49c39963a9c2adf3e
Oleksii Rudenko <oleksii.rudenko@n-fuse.de>
2015-03-12 14:10:23 +0100

    1.0.20

--- app_package/gulpfile.js
+++ app_package/gulpfile.js
@@ -25,7 +25,7 @@ gulp.task('watch', function() {
 });
 
 gulp.task('jspm', function() {
-  return jspm.bundleSFX('main', 'dist/app.js', { sourceMaps: false, minify: true, mangle: true});
+  return jspm.bundleSFX('main', '../dist/app.js', { sourceMaps: false, minify: true, mangle: true});
 });
 
 gulp.task('html', function() {
@@ -33,18 +33,19 @@ gulp.task('html', function() {
     .pipe(rename(function(path) {
       path.basename = path.basename.slice(0, -11); // remove -production
     }))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('../dist'));
 });
 
 gulp.task('css', function() {
   return gulp.src(['vcl.css', 'custom.css'])
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('../dist'));
 });
 
 gulp.task('fonts', function() {
   return gulp.src(['fonts/*'])
-    .pipe(gulp.dest('dist/fonts'));
+    .pipe(gulp.dest('../dist/fonts'));
 });
+
 
 gulp.task('build', ['css', 'html', 'fonts', 'jspm']);
 
@@ -56,14 +57,8 @@ gulp.task('serve-dist', function() {
     online: false,
     port: 3333,
     server: {
-      baseDir: ['./dist']
+      baseDir: ['./../dist']
     }
-  });
-
-  gulp.watch([
-    './**/*'
-  ], function(file){
-    browserSync.reload(file.path);
   });
 
 });
