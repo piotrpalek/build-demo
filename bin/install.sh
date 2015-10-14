4214494a447e34d3b36d490a05cb7f139d13f42e
Oleksii Rudenko <oleksii.rudenko@n-fuse.de>
2015-10-14 14:28:54 +0200

    use babel core

--- bin/install.sh
+++ bin/install.sh
@@ -2,9 +2,14 @@
 
 path=${2:-"../../"}     # path is the target folder. By default it's ../../
 
-node ./index "$path" "$@"    # running against the folder containing node_modules/build-demo
-cd "${path}tmp"         # cd to tmp in there
-npm install             # npm
-npm link jspm gulp gulp-concat gulp-connect gulp-rename gulp-shell gulp-open
-jspm install            # install
-jspm dl-loader          # update loaders
+if [ -d "${path}demo" ]; then
+  # Control will enter here if project/demo exists.
+  node ./index "$path" "$@"    # running against the folder containing node_modules/build-demo
+  cd "${path}tmp"         # cd to tmp in there
+  npm install             # npm
+  npm link jspm gulp gulp-concat gulp-connect gulp-rename gulp-shell gulp-open
+  jspm install            # install
+  jspm dl-loader          # update loaders
+fi
+
+
