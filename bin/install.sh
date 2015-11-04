e2770cc09d1298656832341b0f216c08306cd14a
Oleksii Rudenko <OrKoN@users.noreply.github.com>
2015-11-04 10:10:48 +0100

    link recast

--- bin/install.sh
+++ bin/install.sh
@@ -7,7 +7,7 @@ if [ -d "${path}demo" ]; then
   node ./index "$path" "$@"    # running against the folder containing node_modules/build-demo
   cd "${path}tmp"         # cd to tmp in there
   npm install             # npm
-  npm link jspm gulp gulp-concat gulp-connect gulp-rename gulp-shell gulp-open
+  npm link jspm gulp gulp-concat gulp-connect gulp-rename gulp-shell gulp-open recast
   jspm install            # install
   jspm dl-loader          # update loaders
 fi
